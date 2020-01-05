6576056bd0f73a094c7a6ff9f3130cec04880612
Mark Beamer Jr <markbeamerjr@gmail.com>
2020-01-05 01:14:05 -0500

    fix travis.yml file name

--- app/test/test.go
+++ app/test/test.go
@@ -19,15 +19,15 @@ func RunTests() {
 
 	resp, err := http.Get("http://0.0.0.0:50005/search?s=" + url.QueryEscape("interesting and amazing facts") + "&size=1")
 	if err != nil {
-		logrus.Fatalf("search %s failed with ", "interesting and amazing facts", err)
+		logrus.Fatalf("search %s failed with %s", "interesting and amazing facts", err)
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		logrus.Fatalf("search %s failed with ", "interesting and amazing facts", err)
+		logrus.Fatalf("search %s failed with %s", "interesting and amazing facts", err)
 	}
 	err = json.Unmarshal(body, &results)
 	if err != nil {
-		logrus.Fatalf("search %s failed with ", "interesting and amazing facts", err)
+		logrus.Fatalf("search %s failed with %s", "interesting and amazing facts", err)
 	}
 	logrus.Info(results)
 
