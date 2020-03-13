c61f073aa99882c8a99db67afd1308bb89ec2c32
Mark Beamer Jr <markbeamerjr@gmail.com>
2020-03-12 21:45:51 -0400

    fix linting

--- app/jobs/blocked/blocked.go
+++ app/jobs/blocked/blocked.go
@@ -18,7 +18,7 @@ var blockedChannels = []string{
 	"565be843d5f231d37a037ee6d5276dc1618b5ca3",
 }
 
-// processBlockedList runs through the current blocked list and tries to delete the entry if it exists.
+// ProcessBlockedList runs through the current blocked list and tries to delete the entry if it exists.
 func ProcessBlockedList() {
 	c := lbryinc.NewClient("", nil)
 	r, err := c.Call("file", "list_blocked", nil)
