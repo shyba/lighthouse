49a4c4f72ae0c2365932ef5e3b449fa66c77ad97
Mark Beamer Jr <markbeamerjr@gmail.com>
2020-09-21 01:10:15 -0400

    fix linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -24,7 +24,7 @@ echo "Running errcheck..." && errcheck $(go list ./... | grep -v /vendor/ )
 # check for unnecessary conversions - ignore autogen code
 echo "Running unconvert..." && unconvert -v $(go list ./... | grep -v /vendor/ )
 # checks for function complexity, too big or too many returns
-echo "Running gocyclo..." && gocyclo -ignore "_test" -avg -over 19 $GO_FILES
+echo "Running gocyclo..." && gocyclo -ignore "_test" -avg -over 20 $GO_FILES
 # one last linter - ignore autogen code
 echo "Running golint..." && golint -set_exit_status $(go list ./... | grep -v /vendor/ )
 test $err = 0 # Return non-zero if any command failed
